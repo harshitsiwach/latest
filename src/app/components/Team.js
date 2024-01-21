144643691980e0515678629a872ebbe8cacf359d
NikoSAN02 <dineshbohra@gmail.com>
2024-01-22 00:26:06 +0530

    Updated team section

--- src/app/components/Team.js
+++ src/app/components/Team.js
@@ -1,38 +1,42 @@
 "use client"
 import React, { useState, useEffect } from 'react';
 import Spline from '@splinetool/react-spline';
+import '../Team.css';
 
 function Team() {
-  // URLs of the Spline scenes
-  const splineScenes = [
-    "https://prod.spline.design/GISZpptJtmvD75VN/scene.splinecode",
-    "https://prod.spline.design/eLo1oNfD8BVhQd-b/scene.splinecode",
-    "https://prod.spline.design/GISZpptJtmvD75VN/scene.splinecode",
-    "https://prod.spline.design/eLo1oNfD8BVhQd-b/scene.splinecode",
-    "https://prod.spline.design/GISZpptJtmvD75VN/scene.splinecode",
-    "https://prod.spline.design/eLo1oNfD8BVhQd-b/scene.splinecode",
-    // Add more URLs as needed
-  ];
+ // URLs of the Spline scenes
+ const splineScenes = [
+  "https://prod.spline.design/GISZpptJtmvD75VN/scene.splinecode",
+  "https://prod.spline.design/eLo1oNfD8BVhQd-b/scene.splinecode",
+  "https://prod.spline.design/GISZpptJtmvD75VN/scene.splinecode",
+  "https://prod.spline.design/eLo1oNfD8BVhQd-b/scene.splinecode",
+  "https://prod.spline.design/GISZpptJtmvD75VN/scene.splinecode",
+  "https://prod.spline.design/eLo1oNfD8BVhQd-b/scene.splinecode",
+];
 
-  // State to track the current slide
+  // State to track loading of a new slide
   const [currentSlide, setCurrentSlide] = useState(0);
+  const [loading, setLoading] = useState(false);
 
   useEffect(() => {
-    // Change slide every 3 seconds (3000 milliseconds)
+    // Change slide every 12 seconds
     const timer = setInterval(() => {
-      setCurrentSlide(prevSlide =>
-        prevSlide === splineScenes.length - 1 ? 0 : prevSlide + 1
-      );
-    }, 1500);
+      setLoading(true); // Start fading out
+      setTimeout(() => {
+        setCurrentSlide(prevSlide =>
+          prevSlide === splineScenes.length - 1 ? 0 : prevSlide + 1
+        );
+        setLoading(false); // Fade in the new slide
+      }, 1000); // Start loading new slide after 1 second
+    }, 6000);
 
-    // Clear the timer when the component unmounts
     return () => clearInterval(timer);
   }, []);
 
   return (
-    <main className='w-full bg-black'>
-      <div className='grid grid-cols-1'>
-        <div > {/* Adjust width and height as needed */}
+    <main className='team-container w-full bg-black'>
+      <div className='spline-container'>
+        <div className={`spline ${loading ? 'fade-out' : 'fade-in'}`}>
           <Spline scene={splineScenes[currentSlide]} />
         </div>
       </div>
