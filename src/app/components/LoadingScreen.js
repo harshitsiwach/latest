b80001963a89796dd3d4d97370a1f209559816da
NikoSAN02 <dineshbohra@gmail.com>
2024-01-24 02:04:09 +0530

    Updated for Loading Screen

---
+++ src/app/components/LoadingScreen.js
@@ -0,0 +1,11 @@
+// LoadingScreen.js
+import '../LoadingScreen.css';
+
+const LoadingScreen = () => (
+  <div className="loadingScreen">
+    {/* Your loading message or spinner */}
+    <p>Loading...</p>
+  </div>
+);
+
+export default LoadingScreen;
--- src/app/page.js
+++ src/app/page.js
@@ -1,14 +1,34 @@
+"use client"
 import Homepage from "./components/Homepage";
 import CustomCursor from './components/CustomCursor';
+import React, { useState, useEffect } from 'react';
+import LoadingScreen from '../app/components/LoadingScreen'; // Import the LoadingScreen component
+
 export default function Home() {
+  const [isLoading, setLoading] = useState(true);
+
+  useEffect(() => {
+    // Set a timeout to change the loading state after 5 seconds
+    const timer = setTimeout(() => {
+      setLoading(false);
+    }, 5000);
+
+    // Clear the timer if the component unmounts
+    return () => clearTimeout(timer);
+  }, []);
+
+ 
   return (
     <main>
-    
-      <div className="">
-      
+  <>
+      {isLoading && <LoadingScreen />}
+      <div style={{ visibility: isLoading ? 'hidden' : 'visible' }}>
+        {/* Your main app content */}
       <CustomCursor />
       <Homepage />
       </div>
+    </>   
+      
     </main>
   );
 }
